b3b8117c046976688a1586485cc12eea6e4350d6
oMen ? <omenvng@gmail.com>
2021-02-02 19:47:46 +0200

    JS update

--- JS/function2.js
+++ JS/function2.js
@@ -7,9 +7,13 @@ function MyFunction(){
    {
    counts[arr[i]]=1+(counts[arr[i]] || 0);
        }
-   console.log(counts);
-
+   console.log(counts.length);
 }
 
   
 MyFunction();
+
+let s = new Set();
+const arr= Array.from({length:1000},()=>Math.round(Math.random()*900+1)).forEach((e) => s.add(e)).size;
+
+console.log(arr);
